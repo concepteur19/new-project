9ec626a38db5507d25d6db514d7a9fbfd8f55a0c
concepteur19 <zobel.tchomgui@gmail.com>
2022-12-01 06:41:35 +0100

    react test

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Header from "./Hearder";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
